bcbced930d90addd66b91b528b01fb9111379e60
Matthew Hassell <hassell.matthew@gmail.com>
2016-12-22 17:56:55 -0500

    starting laplace BIEs

--- CalderonCalculusLaplace.py
+++ CalderonCalculusLaplace.py
@@ -1,1 +1,20 @@
 import numpy as np
+
+def CalderonCalculusLaplaceHalf(g,gp):
+    """
+    Input:
+    g:     principle geometry
+    gp:    companion geometry
+
+    Output:
+    V:     single layer operator
+    K:     double layer operator
+    J:     tranposed double layer operator
+    W:     hypersingular operator
+    C:     rank Ncomp perturbation
+
+    last modified: December 21, 2016
+    """
+    pass
+
+
