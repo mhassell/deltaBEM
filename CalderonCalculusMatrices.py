5cc0af8bafa71b5b68c8260cc56bfa4111880168
Matthew Hassell <hassell.matthew@gmail.com>
2016-12-26 16:20:19 -0500

    re-organizing some files and adding the quadrature and mass matrices

---
+++ CalderonCalculusMatrices.py
@@ -0,0 +1,63 @@
+# Quadrature, mass, and mixing matrices
+# Last modified: December 26, 2016
+
+import numpy as np
+import scipy.sparse as sp
+
+def CalderonCalculusMatrices(g,fork=0):
+    
+    """
+    Input:
+    g:     Geometry
+    fork:  (optional)
+
+    Output:
+    Q:     Quadrature matrix
+    M:     Mass Matrix
+    Pp, Pm Mixing matrices
+
+    last modified: December 26, 2016
+    """
+    
+    
+    if fork:
+        alpha = 5.0/6
+    else:
+        alpha = 1.0
+
+    N = g['midpt'].shape[0]
+
+    indicesZeroToN = np.linspace(0,N-1,N)
+
+    # quadrature matrix
+    Q = sp.csr_matrix((22.0/24*np.ones(N),
+                       (indicesZeroToN,indicesZeroToN)),shape=(N,N))+
+    sp.csr_matrix((1./24*np.ones(N),(g['next'],indicesZeroToN)),shape=(N,N))+
+    sp.csr_matrix((1./24*np.ones(N),(indicesZeroToN,g['next'])),shape=(N,N))
+
+    # mass matrix
+    M = sp.csr_matrix(((4.0+12*alpha)/18*np.ones(N),
+                       (indicesZeroToN,indicesZeroToN)),shape=(N,N))+
+    sp.csr_matrix(((7.0-6*alpha)/18*np.ones(N),
+                   (g['next'],indicesZeroToN)),shape=(N,N))+
+    sp.csr_matrix(((7.0-6*alpha)/18*np.ones(N),
+                   (indicesZeroToN,g['next'])),shape=(N,N))
+
+    # mixing/averaging matrices
+    if fork:
+        Pp = sp.csr_matrix((alpha/2,(indicesZeroToN,indicesZeroToN)),size=(N,N))+
+        sp.csr_matrix(((1-alpha)/2, (g['next'],indicesZeroToN)),size=(N,N))
+
+    else:
+        Pp = 0.5
+        Pm = 0.5
+
+    return(Q,M,Pp,Pm)
+    
+    
+
+    
+    
+
+
+
---
+++ laplace.py
@@ -0,0 +1,86 @@
+# operators and potentials for Laplace's equation
+# last modified: December 26, 2016
+
+import numpy as np
+
+def LaplacePotentials(g,z):
+    """
+    Input:
+    g:     geometry
+    z:     observation points (k x 2 array)
+
+    Output:
+    S:     SL matrix
+    D:     DL matrix
+    
+    Last modified: December 24, 2016
+    """
+
+    # SL Pot 
+    DX = z[:,0] - g['midpt'][:,0].T
+    DY = z[:,1] - g['midpt'][:,1].T
+    D = np.sqrt(DX**2 + DY**2)
+    S = -1.0/(2*np.pi)*np.log(D)
+
+    # DL Pot
+    N = DX*g['normal'][:,0].T + DY*g['normal'][:,1].T
+    DL = 1.0/(2*np.pi)*N/(D**2)
+
+    return (S, D)
+
+def _CalderonCalculusLaplaceHalf(g,gp):
+    """
+    Internal function to split up the computation on plus and minus geometries
+    
+    Input:
+    g:     principle geometry
+    gp:    companion geometry
+
+    Output:
+    V:     single layer operator
+    K:     double layer operator
+    J:     tranposed double layer operator
+    W:     hypersingular operator
+     // To be added C:     rank Ncomp perturbation
+
+    last modified: December 24, 2016
+    """
+    # V - SL operator
+
+    DX = gp['midpt'][np.newaxis,0].T - g['midpt'][:,0]
+    DY = gp['midpt'][np.newaxis,1].T - g['midpt'][:,1]
+    D = np.sqrt(DX**2 + DY**2)
+    V = -1.0/(2*np.pi)*np.log(D)
+
+    # K - DL op
+    N = DX*g['normal'][np.newaxis,0].T + DY*g['normal'][:,1].T
+    K = 1.0/(2*np.pi)*N/(D**2)
+
+    # J - transposed DL op
+    N = gp['normal'][np.newaxis,0].T*DX + gp['normal'][np.newaxis,1].T*DY
+    J = -1.0/(2*np.pi)*N/D**2
+
+    # W - hypersingular op
+    DX = gp['brkpt'][np.newaxis,0] - g['brkpt'][np.newaxis,0].T
+    DY = gp['brkpt'][np.newaxis,1] - g['brkpt'][np.newaxis,1].T
+    D = np.sqrt(DX**2 + DY**2)
+    W = -1.0/(2*np.pi)*(np.log(D[gp['next'],g['next'])) + np.log(D)
+                        -np.log(D(gp['next'],:)) - np.log(D(:,g['next'])))
+
+    return (V,K,J,W)
+
+def CalderonCalculusLaplace(g,gp,gm):
+    """
+    Input:
+    g:     principle geometry
+    gp:    plus geometry
+    gm:    minus geometry
+
+    Output:
+    V:
+    K:
+    J:
+    W:
+    """
+    pass
+
